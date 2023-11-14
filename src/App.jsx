caba9efd0730630dfc57444c4a6ff794ada105b5
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2023-11-14 16:28:20 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Box, Container } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <Container maxWidth={"md"}>
+        <Box sx={{ bgcolor: "#cfe8fc", height: "100vh" }} />
+      </Container>
+    </>
+  );
+}
+
+export default App;
